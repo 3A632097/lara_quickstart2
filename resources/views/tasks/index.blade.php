df16abbd0c7a98ae6ef8431f6e0eb843b7faeebd
Ivy Lin <a0981792089@gmail.com>
2019-12-23 00:10:03 +0800

    方法欺騙的註記

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -63,6 +63,8 @@
                                 <form action="/task/{{ $task->id }}" method="POST">
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
+                                    {{--方法欺騙的註記--}}
+                                    <input type="hidden" name="_method" value="DELETE">
 
                                     <button>刪除任務</button>
                                 </form>
